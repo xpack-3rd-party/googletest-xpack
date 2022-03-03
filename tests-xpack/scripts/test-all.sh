83df376d48eec637b504d663c66818371ef1c5da
Liviu Ionescu <ilg@livius.net>
2022-03-03 20:47:06 +0200

    consistent copyright notices

--- tests-xpack/scripts/test-all.sh
+++ tests-xpack/scripts/test-all.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 # -----------------------------------------------------------------------------
-# This file is part of the xPack distribution.
-#   (https://xpack.github.io)
-# Copyright (c) 2020 Liviu Ionescu.
+#
+# This file is part of the µOS++ distribution.
+#   (https://github.com/micro-os-plus/)
+# Copyright (c) 2020 Liviu Ionescu
 #
 # Permission to use, copy, modify, and/or distribute this software
 # for any purpose is hereby granted, under the terms of the MIT license.
+#
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/MIT/.
+#
 # -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
